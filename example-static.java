4ae830dc981d8f0e4fe016853899ca7172fc3027
arrizaqu@yahoo.com <arrizaqu@yahoo.com>
2018-08-01 10:35:05 +0700

    first commit add example static

---
+++ example-static.java
@@ -0,0 +1,15 @@
+public class MainTestStatic {
+    
+    public static void main(String[] args) {
+        MyStatic ms1 = new MyStatic();
+        ms1.address ="jakarta";
+        ms1.name = "Guntur";
+        MyStatic ms2 = new MyStatic();
+        ms2.name = "Agung";
+        ms2.address = "bandung";
+        
+        System.out.println("name : "+ MyStatic.name);
+        System.out.println("address : "+ ms1.address);
+    }
+    
+}
